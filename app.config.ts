673af74557e28e162f2c055f83d26254b79a00e5
raulshma <raulshma@gmail.com>
2026-01-19 21:21:18 +0530

    bump: version

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   slug: "uniarr",
   scheme: "uniarr",
   owner: "throwaway0acc",
-  version: "0.4.3",
+  version: "0.4.4",
   orientation: "portrait",
   icon: "./assets/icon.png",
   newArchEnabled: true,
